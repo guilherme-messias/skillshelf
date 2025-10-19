049cd8db446c08bb4e4002a43291f5785cc87227
Guilherme Messias <messiasguilherme700@gmail.com>
2025-10-19 08:59:50 -0300

    feat: remove arquivos header.php e footer.php da estrutura do frontend

--- views/footer.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Rodapé do site
-?>
-<footer>
-  <p>&copy; 2025 Skillshelf</p>
-</footer>
-</body>
-</html>
--- views/index.php
+++ views/index.php
@@ -1,11 +1,9 @@
-<?php
-include "header.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4" defer></script>
+  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
   <title>Document</title>
 </head>
 <body>
@@ -16,5 +14,3 @@ include "header.php"; ?>
   </main>
 </body>
 </html>
-<?php include "footer.php";
-?>
