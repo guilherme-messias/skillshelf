962652cf5fbf6548d7470583d230d0424b9959cb
Guilherme Messias <messiasguilherme700@gmail.com>
2025-10-19 08:56:26 -0300

    feat: atualiza estrutura do index.php para incluir cabeçalho e rodapé HTML

--- views/index.php
+++ views/index.php
@@ -1,8 +1,20 @@
 <?php
 include "header.php"; ?>
-<main>
-  <h2>Bem-vindo ao Frontend em PHP!</h2>
-  <p>Esta é a nova estrutura do frontend, agora apenas com arquivos PHP.</p>
-</main>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4" defer></script>
+  <title>Document</title>
+</head>
+<body>
+  
+  <main>
+    <h2>Bem-vindo ao Frontend em PHP!</h2>
+    <p>Esta é a nova estrutura do frontend, agora apenas com arquivos PHP.</p>
+  </main>
+</body>
+</html>
 <?php include "footer.php";
 ?>
