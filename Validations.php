059d757f0feb720afc872084547f965343922cbf
Guilherme Messias <messiasguilherme700@gmail.com>
2025-11-12 09:41:12 -0300

    feat: implementa classe de validações com métodos para validação de campos e remove arquivo de configuração TypeScript

--- Validations.php
+++ Validations.php
@@ -1,3 +1,52 @@
 <?php
 
-class Validations {}
+class Validations {
+    public $validations;
+
+    public function validate($rules, $data) {
+        $validation = new self();
+        foreach ($rules as $dataKey => $rulesOfData) {
+            foreach ($rulesOfData as $rule) {
+                $fieldValue = $data[$dataKey];
+                if ($rule == "confirmed") {
+                    $validation->$rule($dataKey, $fieldValue, $data["{$dataKey}_confirmation"]);
+                } elseif (str_contains($rule, ":")) {
+                    $temp = explode(":", $rule);
+
+                    $rule = $temp[0];
+                    $ruleArg = $temp[1];
+
+                    $validation->$rule($ruleArg, $dataKey, $fieldValue);
+                } else {
+                    $validation->$rule($dataKey, $fieldValue);
+                }
+            }
+        }
+        return $validation;
+    }
+
+    private function required($fieldName, $fieldValue) {
+        if (empty($fieldValue)) {
+            $this->validations[] = "O campo {$fieldName} é obrigatório.";
+        }
+    }
+    private function email($fieldName, $fieldValue) {
+        if (!filter_var($fieldValue, FILTER_VALIDATE_EMAIL)) {
+            $this->validations[] = "O campo {$fieldName} deve ser um email válido.";
+        }
+    }
+    private function min($minLength, $fieldName, $fieldValue) {
+        if (strlen($fieldValue) < $minLength) {
+            $this->validations[] = "O campo {$fieldName} deve ter no mínimo {$minLength} caracteres.";
+        }
+    }
+    private function confirmed($fieldName, $fieldValue, $fieldValueConfirmation) {
+        if ($fieldValue !== $fieldValueConfirmation) {
+            $this->validations[] = "O campo {$fieldName} não foi confirmado corretamente.";
+        }
+    }
+    public function fails() {
+      $_SESSION["validationErrors"] = $this->validations;
+      return sizeof($this->validations) > 0;
+    }
+}
