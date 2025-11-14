e4437cd7a422fc706322d7fcc3297c2e3a93cbc1
Guilherme Messias <messiasguilherme700@gmail.com>
2025-11-14 16:55:26 -0300

    feat: adiciona método get à classe Flash para recuperar e remover mensagens de sessão

--- Flash.php
+++ Flash.php
@@ -4,4 +4,14 @@ class Flash {
     public static function push($key, $message) {
         $_SESSION[$key] = $message;
     }
+
+    public static function get($key) {
+        if (!$_SESSION[$key]) {
+            return false;
+        }
+
+        $value = $_SESSION[$key];
+        unset($_SESSION[$key]);
+        return $value;
+    }
 }
