856dcaeea7dd5ba629e2c1dd31bc438076fe6098
Guilherme Messias <messiasguilherme700@gmail.com>
2025-11-19 04:47:55 -0300

    feat: adiciona captura de dados de nota no controlador de itens de conteúdo

--- controllers/user_item_notes.controller.php
+++ controllers/user_item_notes.controller.php
@@ -4,3 +4,7 @@ if (!($_SERVER["REQUEST_METHOD"] === "POST")) {
     header("Location: /login");
     exit();
 }
+
+$content_item_id = $_POST["content_item_id"];
+$note_title = $_POST["note_title"];
+$note_body = $_POST["note_body"];
