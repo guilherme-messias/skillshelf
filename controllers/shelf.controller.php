a9d9752abcff426fd41583e1e184ab6926ad02e8
Guilherme Messias <messiasguilherme700@gmail.com>
2025-11-27 04:56:32 -0300

    feat: atualiza consulta de itens de conteúdo para filtrar por usuário

--- controllers/shelf.controller.php
+++ controllers/shelf.controller.php
@@ -1,9 +1,12 @@
 <?php
 
 $search = $_REQUEST["search"] ?? "";
+$user_id = $_SESSION["user"]->id;
+
 $content_items = $database
-    ->query("SELECT * FROM content_items WHERE title LIKE :search OR source LIKE :search", "Content_item", [
+    ->query("SELECT * FROM content_items WHERE (title LIKE :search OR source LIKE :search) AND user_id = :user_id", "Content_item", [
         ":search" => "%$search%",
+        ":user_id" => $user_id,
     ])
     ->fetchAll();
 
